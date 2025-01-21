bbc7f463f7d31f30bc0a369904ce5ddd261b6eb3
saminmahmud <samin.mahmud18@gmail.com>
2025-01-21 21:39:02 +0600

    add CSRF_TRUSTED_ORIGINS

--- user_account/views.py
+++ user_account/views.py
@@ -38,7 +38,7 @@ class UserRegistrationApiView(APIView):
             # print("token ", token)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
             # print("uid ", uid)
-            confirm_link = f'http://127.0.0.1:8000/accounts/activate/{uid}/{token}'
+            confirm_link = f'https://blogifyon-backend.onrender.com/accounts/activate/{uid}/{token}'
             email_subject = 'Confirm Your Email'
             email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
             email = EmailMultiAlternatives(email_subject, '', to=[user.email])

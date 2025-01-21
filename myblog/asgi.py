692d5d9c0fd701655b22b2a5e40c2b4905b135c0
saminmahmud <samin.mahmud18@gmail.com>
2025-01-21 21:01:23 +0600

    change asgi.py

--- myblog/asgi.py
+++ myblog/asgi.py
@@ -24,18 +24,44 @@
 #     }
 # )
 
+# import os
+
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.security.websocket import AllowedHostsOriginValidator
+# from django.core.asgi import get_asgi_application
+
+
+# import django
+# django.setup()
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
+# from notification.middleware import TokenAuthMiddleware
+# from notification.routing import websocket_urlpatterns
+
+# application = ProtocolTypeRouter(
+#     {
+#         "http": get_asgi_application(),
+#         "websocket": AllowedHostsOriginValidator(
+#             TokenAuthMiddleware(
+#                 AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+#             )
+#         ),
+#     }
+# )
+
+
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
+
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
-
-import django
-django.setup()
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
 from notification.middleware import TokenAuthMiddleware
 from notification.routing import websocket_urlpatterns
 

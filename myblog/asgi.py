800b990c853dcf3387419fab586dbd2b6e3e6d34
saminmahmud <samin.mahmud18@gmail.com>
2025-01-21 19:37:29 +0600

    change asgi.py

--- myblog/asgi.py
+++ myblog/asgi.py
@@ -3,23 +3,21 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-from notification.middleware import TokenAuthMiddleware  # Import the custom middleware
+from notification.middleware import TokenAuthMiddleware 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myblog.settings")
 
 import django
 django.setup()
 
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
+
 from notification.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
+        "http": get_asgi_application(),
         "websocket": AllowedHostsOriginValidator(
-            TokenAuthMiddleware(  # Use the custom TokenAuthMiddleware here
+            TokenAuthMiddleware(  
                 AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
             )
         ),

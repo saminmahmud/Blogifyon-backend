774ff5899e8930efc75ca17bcddf64d09a1560ef
saminmahmud <samin.mahmud18@gmail.com>
2025-02-15 13:43:05 +0600

    added re_path

--- myblog/settings.py
+++ myblog/settings.py
@@ -47,8 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'cloudinary_storage',
-    # 'cloudinary',
     'whitenoise',
     'rest_framework',
     'rest_framework.authtoken',
--- myblog/urls.py
+++ myblog/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('theboss/', admin.site.urls),
     path('accounts/', include('user_account.urls')),
@@ -17,5 +21,5 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
